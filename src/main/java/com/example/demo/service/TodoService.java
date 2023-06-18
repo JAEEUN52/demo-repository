d32375820e4f337bfc72dc0f36e50404f69f0144
JEONG JAEEUN <jje@sookmyung.ac.kr>
2023-06-18 12:05:52 +0900

    백엔드개발 18 @NamedQuery 사용하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -11,13 +11,13 @@ public class TodoService {
 	private TodoRepository repository;
 
 public String testService(){
-	// Todo Entity 생성
-	TodoEntity entity = TodoEntity.builder().UserId("user01").title("My first todo item").build();
-	// Todo Entity 저장
-	repository.save(entity);
-	// Todo Entity 검색
-	TodoEntity savedEntity = 
-	repository.findByUserId(entity.getUserId()).get(0);
-	return savedEntity.getUserId();
-	}
+// Todo Entity 생성
+TodoEntity entity = TodoEntity.builder().userId("JEONG JAEEUN").title("My first todo item").build();
+//Todo Entity 저장
+repository.save(entity);
+//Todo Entity 검색
+TodoEntity savedEntity = 
+repository.searchByUserId(entity.getUserId()).get(0);
+return savedEntity.getUserId();
+}
 }
