ec279ab9b8b01ccf050dc0e663c74ffe911827fd
JEONG JAEEUN <jje@sookmyung.ac.kr>
2023-06-18 11:59:18 +0900

    백엔드개발 17 @Repository에서 쿼리 추가하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -11,12 +11,13 @@ public class TodoService {
 	private TodoRepository repository;
 
 public String testService(){
-// Todo Entity 생성
-TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
-// Todo Entity 저장
-repository.save(entity);
-// Todo Entity 검색
-TodoEntity savedEntity = repository.findById(entity.getId()).get();
-return savedEntity.getTitle();
-}
+	// Todo Entity 생성
+	TodoEntity entity = TodoEntity.builder().UserId("user01").title("My first todo item").build();
+	// Todo Entity 저장
+	repository.save(entity);
+	// Todo Entity 검색
+	TodoEntity savedEntity = 
+	repository.findByUserId(entity.getUserId()).get(0);
+	return savedEntity.getUserId();
+	}
 }
